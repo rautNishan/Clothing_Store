f7ac17ffc8ee907d3c18f1bfb644f5893cfe33e7
Nishan Raut <nishan.raut1709@gmail.com>
2024-04-15 23:39:12 +0545

    Login Design Changed

--- frontend/app/customer/login/page.tsx
+++ frontend/app/customer/login/page.tsx
@@ -1,42 +1,48 @@
 import styles from "./login.module.css";
 import Image from "next/image";
-interface ImgProps {
-  width: number;
-  height: number;
-}
+
 export default function CustomerLogin() {
   return (
-    <div className={styles.login_body_container}>
-      <h3 className={styles.login_text}>Login</h3>
-      <div className={styles.login_container}>
-        <div className={styles.form_container}>
-          <label htmlFor="UserName" className={styles.label}>
-            User Name
-          </label>
-          <input className={styles.input} type="text" />
-          <label htmlFor="Password" className={styles.label}>
-            Password
-          </label>
-          <input className={styles.input} type="text" />
-        </div>
-        <button className={styles.login_button}>Login</button>
-        <label className={styles.text_center}>Or</label>
-        <div className={styles.continue_with_google}>
-          <label className={styles.google_text}>
-            <a href="">Continue with Google</a>
-          </label>
-          <div className={styles.google_img}>
-            <Image
-              src="/svg/icons8-google-48.png"
-              alt="Google"
-              width={22}
-              height={22}
-            />
-          </div>
-        </div>
+    <div className={styles.login_container}>
+      <label className={styles.login_text}>Login</label>
+      <label className={styles.sub_text}>
+        Welcome Back, great to see you again
+      </label>
+      <div className={styles.form_container}>
+        <label htmlFor="UserName" className={styles.label}>
+          User Name
+        </label>
+        <input placeholder="Nishan Raut" className={styles.input} type="text" />
+        <label htmlFor="Password" className={styles.label}>
+          Password
+        </label>
+        <input placeholder="********" className={styles.input} type="text" />
+        <a href="" className={styles.forget_password}>
+          Forget Password?{" "}
+        </a>
       </div>
-     
+      <button className={styles.login_button}>Login</button>
+      <label className={styles.sub_text}>-or-</label>
+      <div className={styles.continue_with_google}>
+        <div className={styles.google_img}>
+          <Image
+            src="/svg/icons8-google-48.png"
+            alt="Google"
+            width={22}
+            height={22}
+          />
+        </div>
+        <label className={styles.google_text}>
+          <a href="">Continue with Google</a>
+        </label>
+      </div>
+      <a className={styles.continue_with_email} href="">
+        Continue With Email
+      </a>
+      <div className={styles.new_to_app}>
+        <label htmlFor="">New to app?</label>
+        <a href="">Create new account</a>
+      </div>
     </div>
   );
 }
-1
